a6a2827511092ff73aea35c3c5fc1b4b8d217852
nhiug <deepanshugarg345@gmail.com>
2026-03-06 17:46:41 +0530

    fix routing for vercel

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,21 +1,37 @@
-import {Route, Routes } from "react-router-dom"
+import { Route, Routes, Navigate } from "react-router-dom"
 import ProductListPage from "../../features/products/pages/ProductListPage"
 import ProductDetailPage from "../../features/products/pages/ProductDetailPage"
 
+function AppRouter() {
+  return (
+    <Routes>
 
-function AppRouter(){
-    return(
+      {/* homepage */}
+      <Route
+        path="/"
+        element={<ProductListPage />}
+      />
 
-            <Routes>
-                <Route 
-                path="/products"
-                element = {<ProductListPage />}
-                />
+      {/* products page */}
+      <Route
+        path="/products"
+        element={<ProductListPage />}
+      />
 
-                <Route path="/product/:id" element = {< ProductDetailPage/>}/>
-            </Routes>
-  
-    )
+      {/* product detail */}
+      <Route
+        path="/product/:id"
+        element={<ProductDetailPage />}
+      />
+
+      {/* fallback */}
+      <Route
+        path="*"
+        element={<Navigate to="/" replace />}
+      />
+
+    </Routes>
+  )
 }
 
 export default AppRouter
